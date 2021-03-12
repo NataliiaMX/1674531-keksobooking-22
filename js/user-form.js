f6b889e0d515c3c6e569bf5b9b8e608715d83cee
Nataliia <natalia.egorova978@gmail.com>
2021-03-12 21:24:16 +0400

    Изменение минимальной цены итд

--- js/user-form.js
+++ js/user-form.js
@@ -29,3 +29,46 @@ resetButton.addEventListener('click', () =>
 function clearForm () {
   userForm.reset();
 }
+
+//changing minimun price for listing type
+
+const type = document.querySelector('#type');
+const price = document.querySelector('#price');
+
+type.addEventListener ('change', (evt) => {
+  if (evt.target.selectedOptions[0].textContent === 'Бунгало') {
+    price.setAttribute('min', 0);
+  }
+  else if (evt.target.selectedOptions[0].textContent === 'Квартира') {
+    price.setAttribute('min', 1000);
+  }
+  else if (evt.target.selectedOptions[0].textContent === 'Дом') {
+    price.setAttribute('min', 5000);
+  }
+  else if (evt.target.selectedOptions[0].textContent === 'Дворец') {
+    price.setAttribute('min', 10000);
+  }
+})
+
+//sync timein and timeout
+
+const timeIn = document.querySelector('#timein');
+const timeOut = document.querySelector('#timeout');
+
+timeChange(timeIn, timeOut);
+timeChange(timeOut, timeIn);
+
+
+function timeChange (timeOne, timeTwo) {
+  timeOne.addEventListener('change', (evt) => {
+    if (evt.target.selectedOptions[0].value === '12:00') {
+      timeTwo.options[0].setAttribute('selected', 'selected');
+    }
+    else if (evt.target.selectedOptions[0].value === '13:00') {
+      timeTwo.options[1].setAttribute('selected', 'selected');
+    }
+    else if (evt.target.selectedOptions[0].value === '14:00') {
+      timeTwo.options[2].setAttribute('selected', 'selected');
+    }
+  })
+}
